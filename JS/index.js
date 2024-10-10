d1bff6b0004d0bc81a7e496756b723ee8674313c
ppsegur <pepesegurarodriguez@gmail.com>
2024-10-10 08:57:26 +0200

    CAmbio de API para la utilización de imágenes

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,45 @@
+
+
+
+$(document).ready(function () {
+  getStarWarsCharacters();
+
+  function getStarWarsCharacters() {
+      $("#data-content").append('<div id="loading" class="text-center my-3"><img src="../IMG/carga.gif" style="width: 50px;" /></div>');
+
+      $.ajax({
+          url: `https://akabab.github.io/starwars-api/api/all.json`,
+          method: "GET",
+      }).done(function (resp) {
+          setTimeout(function () {
+              $("#loading").remove();
+
+              resp.forEach(function (character) {
+                  var template = `
+                    <div class="col-md-4 mb-4">
+                      <div class="card h-100">
+                        <img src="${character.image}" class="card-img-top" alt="${character.name}">
+                        <div class="card-body">
+                          <h5 class="card-title">${character.name}</h5>
+                          <p class="card-text">Altura: ${character.height} cm, Peso: ${character.mass} kg</p>
+                          <a href="${character.wiki}" class="btn btn-dark" target="_blank">Ver detalles</a>
+                        </div>
+                      </div>
+                    </div>
+                  `;
+                  $("#data-content").append(template);
+              });
+
+              $("#btn-get-data").addClass("d-none");
+
+          }, 1000);
+      }).fail(function () {
+          $("#loading").remove();
+          $("#data-content").append("<p>Error al cargar los personajes. Inténtalo de nuevo más tarde.</p>");
+      });
+  }
+});
+/*
 $(document).ready(function () {
     let currentPage = 1;
   
@@ -14,7 +56,8 @@ $(document).ready(function () {
       $("#data-content").append('<div id="loading" class="text-center my-3"><img src="../IMG/carga.gif" style="width: 50px;" /></div>'); // Imagen de carga pequeña
   
       $.ajax({
-        url: `https://swapi.dev/api/people/?page=${page}`, // Cambia la página dinámica
+        url: `https://akabab.github.io/starwars-api/api/all.json`, // Cambia a la nueva API con imágenes 
+       // url: `https://swapi.dev/api/people/?page=${page}`, 
         method: "GET",
       }).done(function (resp) {
         setTimeout(function () {
@@ -27,6 +70,18 @@ $(document).ready(function () {
           charactersList.forEach(function (character) {
             var characterId = character.url.split("/")[5]; // Extrae el ID del personaje desde la URL
             var template = `
+            <div class="col-md-4 mb-4">
+              <div class="card h-100">
+                <img src="${character.image}" class="card-img-top" alt="${character.name}">
+                <div class="card-body">
+                  <h5 class="card-title">${character.name}</h5>
+                  <p class="card-text">Altura: ${character.height} cm, Peso: ${character.mass} kg</p>
+                  <a href="${character.wiki}" class="btn btn-dark" target="_blank">Ver detalles</a>
+                </div>
+              </div>
+            </div>
+          `;
+            /*var template = `
               <div class="col-md-4 mb-4">
                 <div class="card h-100">
                   <div class="card-body">
@@ -36,9 +91,9 @@ $(document).ready(function () {
                   </div>
                 </div>
               </div>
-            `;
+            `;*/
             // Añade el contenido de cada personaje al contenedor principal
-            $("#data-content").append(template);
+           /* $("#data-content").append(template);
           });
   
           // Mostrar el botón de cargar más después de las tarjetas
@@ -50,4 +105,4 @@ $(document).ready(function () {
         $("#data-content").append("<p>Error al cargar los personajes. Inténtalo de nuevo más tarde.</p>");
       });
     }
-  });
+  });*/
